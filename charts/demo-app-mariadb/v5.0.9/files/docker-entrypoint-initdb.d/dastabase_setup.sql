141c57c315ea1fd9aaaf1fdcb319b00de40f4ba6
Alexander Kharkevich <Alexander_Kharkevich@epam.com>
2019-10-02 15:09:50 +0300

    update init script

--- charts/demo-app-mariadb/v5.0.9/files/docker-entrypoint-initdb.d/dastabase_setup.sql
+++ charts/demo-app-mariadb/v5.0.9/files/docker-entrypoint-initdb.d/dastabase_setup.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE messagesdb;
-CREATE USER proxyuser IDENTIFIED BY '***';
-GRANT INSERT, SELECT, UPDATE ON messagesdb.* to 'proxyuser'@'%';
+CREATE DATABASE IF NOT EXISTS `messagesdb`;
+-- CREATE USER proxyuser IDENTIFIED BY '***';
+-- GRANT INSERT, SELECT, UPDATE ON messagesdb.* to 'proxyuser'@'%';
 
-USE messagesdb;
+USE `messagesdb`;
 
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS `messages` (
   id INT AUTO_INCREMENT PRIMARY KEY, 
   user_from VARCHAR(50) NOT NULL,
   user_to VARCHAR(50) NOT NULL,
   text TEXT
-);
+) DEFAULT CHARSET=utf8;
